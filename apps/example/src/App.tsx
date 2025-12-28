c923516ed20798e37d999038758a9ae489badb26
MahibulHaque <mahibulhaque2000@gmail.com>
2025-12-28 17:58:10 +0600

    chore: update example app

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -5,18 +5,69 @@
  * @format
  */
 
-import { StatusBar, useColorScheme } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 function App() {
-    const isDarkMode = useColorScheme() === "dark";
+  const isDarkMode = useColorScheme() === "dark";
 
-    return (
-        <SafeAreaProvider>
-            <StatusBar
-                barStyle={isDarkMode ? "light-content" : "dark-content"}
-            />
-        </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
 }
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: safeAreaInsets.top,
+        paddingLeft: safeAreaInsets.left,
+        paddingRight: safeAreaInsets.right,
+      }}
+    >
+      <ScrollView style={{ paddingBottom: safeAreaInsets.bottom }}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Nitro Sms Example</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: "center",
+    paddingHorizontal: 24,
+  },
+  header: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 64,
+    marginBottom: 48,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 24,
+  },
+});
